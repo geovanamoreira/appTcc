f8ed7e496395caca61cde283db846bae882b37bc
nmicadei <61022310+nmicadei@users.noreply.github.com>
2022-06-14 11:59:28 +0000

    validação e mensagens de erro

--- src/app/login-empregado/login-empregado.page.ts
+++ src/app/login-empregado/login-empregado.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { comparaValidator } from '../validators/compara-validator';
 
 @Component({
   selector: 'app-login-empregado',
@@ -8,11 +9,42 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class LoginEmpregadoPage implements OnInit {
 
-  formCadastro: FormGroup;
+  formLogin1: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  mensagens = {
+    nome: [
+      { tipo: 'required', mensagem: 'O nome é obrigatório' },
+      { tipo: 'minlength', mensagem: 'O nome deve haver pelo menos 3 caracteres' }
+    ],
+
+    senha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+    ],
+   confirmaSenha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+      { tipo: 'comparacao', mensagem: 'Deve ser igual a senha.' },
+    ],
+  };
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formLogin1 = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      confirmaSenha: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    }, {
+      validator: comparaValidator('senha', 'confirmaSenha')
+    });
+  }
 
   ngOnInit() {
+  }
+
+  salvarLogin() {
+    console.log('Formulário: ', this.formLogin1.valid);
   }
 
 }
